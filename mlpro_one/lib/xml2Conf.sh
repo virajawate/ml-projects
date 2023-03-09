2870e7df37dea0efdc358557a9b76915c268d31f
virajawate <viraj.awate@gmail.com>
2023-03-10 00:54:02 +0530

    EDA-notebook

---
+++ mlpro_one/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/viraj/python/Python/Machine-learning/mlpro_one/lib"
+XML2_LIBS="-lxml2 -L/home/viraj/python/Python/Machine-learning/mlpro_one/lib -lz -L/home/viraj/python/Python/Machine-learning/mlpro_one/lib -llzma -lpthread -L/home/viraj/python/Python/Machine-learning/mlpro_one/lib -L/home/viraj/python/Python/Machine-learning/mlpro_one/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/viraj/python/Python/Machine-learning/mlpro_one/include/libxml2 -I/home/viraj/python/Python/Machine-learning/mlpro_one/include"
+MODULE_VERSION="xml2-2.9.14"
+
--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -16,3 +17,11 @@ class CustomException(Exception):
     def __str__(self) -> str:
         return self.error_msg
 
+# Trial Error  
+# if __name__ =="__main__":
+    
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info("Dividing by zero")
+#         raise CustomException(e, sys)
